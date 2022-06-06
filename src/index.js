93e0e2e616c9f737fa835f40899e22fba50dde00
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-06-06 14:53:17 +0200

    updated React 17 to 18 changing adopting root.render in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom";
 import { App } from "./App";
 
-const root = document.querySelector('#root')
-ReactDOM.render(<App />, root)
+const container = document.querySelector('#root')
+const root = ReactDOM.createRoot(container);
+root.render (<App/>);
