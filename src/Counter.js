3d1ff2d6110b04a109532e61ece3b71029cf2be1
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-06-08 18:00:10 +0200

    Es 1 Completed

--- src/Counter.js
+++ src/Counter.js
@@ -5,17 +5,17 @@ export class Counter extends React.Component{
     state = {
         count:0
     }
-    constructor(props) {
-        super(props)
-
-        setInterval(() => {
-        this.setState((state) => {
-            return {
-                count: state.count+1
-            }
-        })
-    }, this.props.incrementTimer)
-    }
+    
+    componentDidMount() {
+		setInterval(() => {
+			this.setState((state) => {
+				return {
+					count: state.count + 1
+				}
+			})
+		}, 1000)
+	}
+    
     render () {
         return (
             <div>
