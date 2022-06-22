8955b4a3ea15bb26a2ed37196a2733293fac440d
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-06-22 13:21:12 +0200

    ComponentComposition ex 2 completed

--- src/Container.js
+++ src/Container.js
@@ -2,6 +2,11 @@ import React from "react";
 
 export class Container extends React.Component {
 	render() {
-		return <div className="containerStyle">{this.props.children}</div>;
+		return (
+			<div>
+				<div>{this.props.title}</div>
+				<div className="containerStyle">{this.props.children}</div>
+			</div>
+		);
 	}
 }
