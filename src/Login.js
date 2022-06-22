f15d50073aa4da1526fee42d6d0c24793d9c60ab
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-06-22 21:58:55 +0200

    useState ex 2 completed

--- src/Login.js
+++ src/Login.js
@@ -1,69 +1,43 @@
-import React from "react";
+import { useState } from "react";
 
-export class Login extends React.Component {
-	state = {
+export function Login() {
+	const [data, setData] = useState({
 		username: "",
 		password: "",
-		remember: false,
-	};
+		remember: "false",
+	});
 
-	handleInputChange = (event) => {
-		const name = event.target.name;
-		const value = event.target.value;
-		const type = event.target.type;
-		const checked = event.target.checked;
+	function handleInputChange(event) {
+		const { name, type, value, checked } = event.target;
 
-		this.setState({
-			[name]: type === "checkbox" ? checked : value,
+		setData((data) => {
+			return {
+				...data,
+				[name]: type === "checkbox" ? checked : value,
+			};
 		});
-	};
-
-	LoginButtonClick = () => {
-		this.props.onLogin(this.state);
-	};
-
-	ResetForm = () => {
-		this.setState({
-			username: "",
-			password: "",
-			remember: false,
-		});
-	};
-
-	render() {
-		const MyStyle = {
-			backgroundColor: this.state.password.length < 8 ? "red" : "green",
-		};
-		return (
-			<div>
-				<input
-					name="username"
-					value={this.state.username}
-					onChange={this.handleInputChange}
-				/>
-				<input
-					name="password"
-					type="password"
-					value={this.state.password}
-					onChange={this.handleInputChange}
-				/>
-				<input
-					name="remember"
-					type="checkbox"
-					checked={this.state.remember}
-					onChange={this.handleInputChange}
-				/>
-
-				<button
-					style={MyStyle}
-					disabled={!this.state.username || !this.state.password}
-					onClick={this.LoginButtonClick}
-				>
-					Login
-				</button>
-
-				<button onClick={this.ResetForm}>Reset</button>
-			</div>
-		);
+		console.log(data);
 	}
+
+	return (
+		<form>
+			<input
+				onChange={handleInputChange}
+				value={data.username}
+				name="username"
+			/>
+			<input
+				onChange={handleInputChange}
+				value={data.password}
+				type="password"
+				name="password"
+			/>
+			<input
+				onChange={handleInputChange}
+				value={data.remember}
+				type="checkbox"
+				name="checkbox"
+			/>
+		</form>
+	);
 }
