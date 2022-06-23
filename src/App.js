4faaf08da9deac0d566df072cdf8c4b49e09b76e
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-06-23 03:12:55 +0200

    UseEffect ex 3 completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Login } from "./Login";
+import { GithubUser } from "./GithubUser";
 
 export class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Login />
+				<GithubUser username="ColomboRiccardo" />
 			</div>
 		);
 	}
