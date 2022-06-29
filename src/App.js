d1a7f432f6d846f625ef0b8de3112c0a2de710cf
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-06-30 01:03:12 +0200

    React Router ex 4 completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter";
 import { ShowGithubUser } from "./ShowGithubUser";
@@ -8,6 +8,13 @@ export class App extends React.Component {
 	render() {
 		return (
 			<div>
+				<h1>Choose your path:</h1>
+				<Link to="/">Home</Link>
+				<hr />
+				<Link to="/counter">Counter</Link>
+				<hr />
+				<Link to="users/:username">Username Selector</Link>
+				<hr />
 				<Routes>
 					<Route path="/" element={<Welcome name="Mauro" />} />
 					<Route path="counter" element={<Counter />} />
