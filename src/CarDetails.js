aea61821cd4d7287551b916d158de79d313c32b2
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-06-29 20:04:30 +0200

    useRef ex 2 completed

---
+++ src/CarDetails.js
@@ -0,0 +1,21 @@
+import { useRef, useEffect } from "react";
+
+export function CardDetails({
+	initialData = { model: "Fiat Panda", year: "1980", color: "red" },
+}) {
+	const { model, year, color } = initialData;
+
+	const formRef = useRef(null);
+
+	useEffect(() => {
+		formRef.current.reset();
+	}, [initialData]);
+
+	return (
+		<form ref={formRef}>
+			<input name="model" defaultValue={model}></input>
+			<input name="year" defaultValue={year}></input>
+			<input name="color" defaultValue={color}></input>
+		</form>
+	);
+}
