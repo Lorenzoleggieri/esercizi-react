35385d504ee15e15e86d96ebf14e16240e6208f8
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-06-30 00:23:08 +0200

    React Router ex 2 Completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import { GithubUser } from "./GithubUser";
 import { GithubUserList } from "./GithubUserList";
 import { Welcome } from "./Welcome";
+import { Counter } from "./Counter";
 
 export class App extends React.Component {
 	render() {
 		return (
 			<Routes>
 				<Route path="/" element={<Welcome name="Mauro" />} />
+				<Route path="/counter" element={<Counter />} />
 			</Routes>
 		);
 	}
