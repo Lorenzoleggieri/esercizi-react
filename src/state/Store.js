1598675fe7514769edda6523ec01a00b77ff5b6c
lorenzo leggieri <lorenzoleggieri@gmail.com>
2022-07-01 15:00:15 +0200

    Redux ex 4 completed

--- src/state/Store.js
+++ src/state/Store.js
@@ -1,15 +1,31 @@
-import { configureStore } from "redux";
+import { configureStore, combineReducers } from "redux";
 import {
 	counterReducer,
 	decrementCounter,
 	incrementCounter,
 	resetCounter,
 } from "./CounterState";
+import {
+	todosReducer,
+	removeTodo,
+	addTodo,
+	resetTodos,
+	editUser,
+} from "./TodosState";
 
-export const store = configureStore(counterReducer);
+const rootReducer = combineReducers({
+	counter: counterReducer,
+	todos: todosReducer,
+});
+
+export const store = configureStore(rootReducer);
 
 store.subscribe(() => {
 	console.log(store.getState());
 });
 
 store.dispatch(incrementCounter(42));
+store.dispatch(addTodo({ id: 1, title: "compra pane", completed: true }));
+store.dispatch(removeTodo(1));
+store.dispatch(decrementCounter(12));
+store.dispatch(editUser(1, { title: "compra pasta", completed: false }));
